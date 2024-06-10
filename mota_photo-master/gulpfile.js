fbb389e78cd539d0cc19eb559128e0717650c0d8
SiRipo92 <ripochesierra@gmail.com>
2024-06-10 13:42:19 +0200

    Added sass file dependencies to lint, compile and watch sass files in gulpfile.js

--- mota_photo-master/gulpfile.js
+++ mota_photo-master/gulpfile.js
@@ -1,15 +1,24 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sassLint = require('gulp-sass-lint');
+
+function lintSass() {
+  return src('sass/**/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+}
 
 function compileSass() {
-  return src('sass/custom.scss')
+  return src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('assets/css/custom.css'));
+    .pipe(dest('/assets/css/'));
 }
 
 function watchFiles() {
-  watch('sass/scss/**/custom.scss', compileSass);
+  watch('sass/**/*.scss', compileSass);
 }
 
 exports.default = series(compileSass, watchFiles);
 exports.watchFiles = watchFiles;
+exports.lint = lintSass;
