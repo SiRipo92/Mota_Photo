c548ffd4cd81453ec7dc21e924d1ae03a37c8cd1
SiRipo92 <ripochesierra@gmail.com>
2024-06-13 11:53:17 +0200

    updated sass installation setup to search for only 'custom.scss' to compile and output in 'assets/css/custom.css'. Haven't tested the watch/compile functions, but the lint works.

--- motaphoto/gulpfile.js
+++ motaphoto/gulpfile.js
@@ -4,22 +4,24 @@ const sassLint = require('gulp-sass-lint');
 const sourcemaps = require('gulp-sourcemaps');
 
 function lintSass() {
-  return src('sass/**/*.scss')
-    .pipe(sassLint())
+  return src('sass/custom.scss')
+    .pipe(sassLint({
+      configFile: '.sass-lint.yml' // Ensure you have a sass-lint configuration file
+    }))
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError());
 }
 
 function compileSass() {
-  return src('sass/**/*.scss')
+  return src('sass/custom.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
-    .pipe(dest('./'));
+    .pipe(dest('assets/css')); // Output to the css directory
 }
 
 function watchFiles() {
-  watch('sass/**/*.scss', series(lintSass, compileSass));
+  watch('sass/custom.scss', series(lintSass, compileSass));
 }
 
 exports.default = series(lintSass, compileSass, watchFiles);
