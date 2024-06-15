421579d46c46c14697aac44ab8d714980e756741
SiRipo92 <ripochesierra@gmail.com>
2024-06-15 20:40:11 +0200

    footer nav menu updated to include walker and menu classes and ids

--- motaphoto/footer.php
+++ motaphoto/footer.php
@@ -1,13 +1,14 @@
         <footer>
             <?php wp_footer(); ?>
-            <nav role="navigation" aria-label="<?php _e('Menu footer', 'footer'); ?>">
-            <?php wp_nav_menu(array
-            ('theme_location' => 'footer',
-            'container' => false,
-            'menu_id' => 'footer-menu',
-            'menu_class' => 'footer-menu',
-            'walker' => new Motaphoto_Nav_Walker()
-            )); ?>
+            <nav class="footer-nav"role="navigation" aria-label="<?php _e('Menu footer', 'footer'); ?>">
+                <?php wp_nav_menu(array
+                ('theme_location' => 'footer',
+                'container' => false,
+                'menu_id' => 'footer-menu',
+                'menu_class' => 'footer-menu',
+                'walker' => new Motaphoto_Nav_Walker()
+                )); ?>
+            </nav>
         </footer>
     </body>
 </html>
