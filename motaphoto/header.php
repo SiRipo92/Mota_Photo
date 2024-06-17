cfdcee2c28f2f04560b14907e33a936c5d2f8a36
SiRipo92 <ripochesierra@gmail.com>
2024-06-17 15:54:08 +0200

    burger menu is in place at less than 768 px and both menus are behaving properly in responsive design in both mobile and desktop versions. Burger menu still needs javascript and specific design when opened.

--- motaphoto/header.php
+++ motaphoto/header.php
@@ -10,59 +10,31 @@
 </head>
 <body <?php body_class(); ?>>
     <div id="page" class="site-main">
-        <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'textdomain'); ?></a>
+        <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'MotaPhoto'); ?></a>
         <header id="masthead" class="site-header">
-            <div class="nav-menu">
-                <div class="site-branding">
-                    <div class="logo-container">
-                        <?php the_custom_logo(); ?>
-                    </div>
-                </div><!-- .site-branding -->
-
-                <nav role="navigation" aria-label="<?php _e('Main Menu', 'textdomain'); ?>" class="main-nav" id="main-menu">
-                    <?php
-                    wp_nav_menu(
-                        array(
-                            'theme_location' => 'main-menu',
-                            'menu_id' => 'primary-menu',
-                            'menu_class' => 'primary-menu',
-                            'container' => false,
-                            'walker' => new Motaphoto_Nav_Walker(),
-                        )
-                    );
-                    ?>
-                </nav>
-
-                <div class="burger close-button">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+            <div class="logo-container">
+                <?php the_custom_logo(); ?>
             </div>
-            <div id="mobile-menu" class="mobile-menu">
-            <div class="mobile-header">
-                <div class="site-branding">
-                    <div class="logo-container">
-                        <?php the_custom_logo(); ?>
-                    </div>
-                </div>
-            </div>
+            <nav id="header-navigation" class="main-navigation" role="navigation" aria-label="<?php _e('Main Menu', 'MotaPhoto'); ?>">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-menu',
+                        'menu_id' => 'primary-menu',
+                        'menu_class' => 'primary-menu',
+                        'container' => false,
+                        'walker' => new Motaphoto_Nav_Walker(),
+                    )
+                );
+                ?>
+            </nav>
             <div class="mobile-menu-container">
-                <nav role="navigation" aria-label="<?php _e('Mobile Menu', 'textdomain'); ?>" class="mobile-nav">
-                    <?php
-                    wp_nav_menu(
-                        array(
-                            'theme_location' => 'main-menu',
-                            'menu_id' => 'mobile-main-menu',
-                            'menu_class' => 'mobile-menu-nav',
-                            'container' => false,
-                            'walker' => new Motaphoto_Nav_Walker(),
-                        )
-                    );
-                    ?>
-                </nav>
+                <button class="burger-btn btn-close">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                </button> 
             </div>
-        </div>
         </header><!-- #masthead -->
 
         <div id="content" class="site-content">
