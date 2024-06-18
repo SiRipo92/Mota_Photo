96a4adf357eeee57ee7e7d408b61ce346dc11b96
SiRipo92 <ripochesierra@gmail.com>
2024-06-18 12:36:22 +0200

    finished creating contact modal and its form elements

---
+++ motaphoto/template-parts/modal-contact.php
@@ -0,0 +1,18 @@
+<div id="contact-modal" class="modal">
+    <div class="modal-container">
+        <div class="modal-contact_btn-close">
+            <button class="btn-close">
+                    <span class="close-bar"></span>
+                    <span class="close-bar"></span>
+            </button>
+        </div>
+        <div class="modal-content">
+            <div class="modal-content__header">
+                <img id="contact-banner" src="<?php echo get_template_directory_uri() . '/assets/images/nav/contact-header-bg.png' ; ?>" alt="Contact form banner">
+            </div>
+            <div class="modal-content__form">
+                <?php echo do_shortcode('[contact-form-7 id="68dcfdf" title="Modal contact"]'); ?>
+            </div>
+        </div>
+    </div>
+</div>
