3284de622daa2db470085e86d7102805855359c2
SiRipo92 <ripochesierra@gmail.com>
2024-06-18 15:50:25 +0200

    reorganised sass files to include a layout/ folder and styled my hero-header image into the page, integrated this element into my index.php

--- motaphoto/index.php
+++ motaphoto/index.php
@@ -1,8 +1,11 @@
 <?php get_header(); ?>
 
 <main id="site-content" role="main">
-
-
+    <section id="hero-header">
+        <div class="hero-banner">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/hero-header-bg.png' ; ?>" >
+        </div>
+    </section>
 </main><!-- #site-content -->
 
 <?php get_sidebar(); ?>
