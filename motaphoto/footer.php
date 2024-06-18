5b846036dc8bb534934d3855b9f46fa66cb82573
SiRipo92 <ripochesierra@gmail.com>
2024-06-18 12:32:52 +0200

    updated footer.php to include modal-contact partial template

--- motaphoto/footer.php
+++ motaphoto/footer.php
@@ -1,14 +1,16 @@
-        <footer>
-            <?php wp_footer(); ?>
-            <nav class="footer-nav"role="navigation" aria-label="<?php _e('Menu footer', 'footer'); ?>">
-                <?php wp_nav_menu(array
-                ('theme_location' => 'footer',
-                'container' => false,
-                'menu_id' => 'footer-menu',
-                'menu_class' => 'footer-menu',
-                'walker' => new Motaphoto_Nav_Walker()
-                )); ?>
-            </nav>
-        </footer>
-    </body>
+<!-- footer.php -->
+<footer>
+    <nav class="footer-nav" role="navigation" aria-label="<?php _e('Menu footer', 'footer'); ?>">
+        <?php wp_nav_menu(array(
+            'theme_location' => 'footer',
+            'container' => false,
+            'menu_id' => 'footer-menu',
+            'menu_class' => 'footer-menu',
+            'walker' => new Motaphoto_Nav_Walker()
+        )); ?>
+    </nav>
+    <?php get_template_part('template-parts/modal-contact'); ?>
+    <?php wp_footer(); ?>
+</footer>
+</body>
 </html>
