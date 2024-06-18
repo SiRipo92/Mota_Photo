4a884c18e5dfaa01121e48de412421f5e4e8e98e
SiRipo92 <ripochesierra@gmail.com>
2024-06-18 12:34:58 +0200

    Added code to manage open/closing of contact modal

--- motaphoto/assets/js/index.js
+++ motaphoto/assets/js/index.js
@@ -1,9 +1,25 @@
+// /js/scripts.js
 jQuery(document).ready(function($) {
+    // Manage mobile menu open/close
     $('.burger-btn').on('click', function() {
         $('.mobile-menu').toggleClass('open');
         $(this).toggleClass('btn-close');
         // Toggle aria-expanded attribute
         var expanded = $(this).attr('aria-expanded') === 'true' || false;
         $(this).attr('aria-expanded', !expanded);
+    });
+
+    // Manage contact-link modal open/close
+    $('#contactLink').on('click', function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        $('#contact-modal').fadeIn();
+    });
+
+    $('.btn-close, .modal').on('click', function() {
+        $('#contact-modal').fadeOut();
+    });
+
+    $('.modal-container').on('click', function(event) {
+        event.stopPropagation();
     });
 });
