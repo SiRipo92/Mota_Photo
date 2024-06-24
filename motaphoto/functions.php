1d8b0924d84636c7dfa1ef1510d9432163953b65
SiRipo92 <ripochesierra@gmail.com>
2024-06-24 12:49:50 +0200

    localized script for ajax photo-navigation (but this logic needs to be fixed to not change the photo but display the thumbnail of the next post and use the buttons to change page -- NOT the photo.

--- motaphoto/functions.php
+++ motaphoto/functions.php
@@ -28,6 +28,11 @@ function add_motaphoto_scripts() {
     wp_enqueue_script('jquery', array('jquery'), null, true);
     wp_enqueue_script('motaphoto-custom-script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), null, true);
 
+    wp_enqueue_script('photo-navigation', get_template_directory_uri() . '/assets/js/photo-navigation.js', array('jquery'), null, true);
+    wp_localize_script('photo-navigation', 'myScriptData', array(
+        'ajaxurl' => admin_url('admin-ajax.php') 
+    ));
+
     // Assuming you're on a single custom post type page
     if (is_singular('photo')) {
         $referenceID = get_field('Reference', get_the_ID());
@@ -37,6 +42,10 @@ function add_motaphoto_scripts() {
     }
 }
 add_action('wp_enqueue_scripts', 'add_motaphoto_scripts');
+
+// Include custom AJAX handlers
+require_once get_template_directory() . '/inc/ajax-handlers.php';
+
 /*
 ******************* Add theme support for custom header
 */
@@ -102,7 +111,3 @@ function register_navwalker(){
     require_once get_template_directory() . '/inc/menus.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
-
-
-
-
